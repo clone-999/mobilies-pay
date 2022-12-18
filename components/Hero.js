b831e0437f2f48796f0455ce84edf3d99b4f3660
clone-999 <dbako2000@gmail.com>
2022-12-18 18:10:51 +0400

    Residecies picture updated

--- components/Hero.js
+++ components/Hero.js
@@ -9,14 +9,14 @@ const Hero = () => {
             <div className="row">
                 <div className="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" >
                     <h1>Tahmeen Document Clearing Services LLC</h1>
-                    <h2>Official page for Dubai Residences payment. Please make your payments on this page only</h2>
+                    <h2>Official page for Dubai Residences payment. Please make your payments on this page only. Also scroll below for more information</h2>
                     <div className="d-flex justify-content-center justify-content-lg-start">
                         <Link href="#contact" className="btn-get-started scrollto">Get Started</Link>
                         <Link href="#contact" className="glightbox btn-watch-video"><i className="bi bi-play-circle"></i><span>Pay Here</span></Link>
                     </div>
                 </div>
                 <div className="col-lg-6 order-1 order-lg-2 hero-img">
-                    <img src="/assets/img/hero-img.png" className="img-fluid animated" alt="" />
+                    <img src="/assets/img/emirates-id.png" className="img-fluid animated" alt="" />
                 </div>
             </div>
         </div>
